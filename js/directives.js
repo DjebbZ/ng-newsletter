0442c45e2b5fb53bd75f6c909da36b6a965f2591
DjebbZ <khalid.jebbari@gmail.com>
2013-12-18 15:10:01 +0100

    Add a requirement and a model to the directive, taken from an input

--- js/directives.js
+++ js/directives.js
@@ -2,6 +2,10 @@ angular.module('directives', [])
     .directive('ngSparkline', function() {
         return {
             restrict: 'AECM',
+            require: '^ngModel',
+            scope: {
+                ngModel: '='
+            },
             templateUrl: 'views/ng-sparkline.html'
         }
     })
