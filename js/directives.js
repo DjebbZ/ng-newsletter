70ee799c4f55cecd14ffb2e6bd806cc80a04d376
DjebbZ <khalid.jebbari@gmail.com>
2013-12-18 17:38:19 +0100

    Display a chart using d3.js

--- js/directives.js
+++ js/directives.js
@@ -38,8 +38,53 @@ angular.module('directives', [])
                         angular.forEach(scope.weather, function(value, key){
                             highs.push(value.temp.max)
                         });
+                        chartGraph(iElement, highs, iAttrs)
                     }
                 });
             }
         }
     })
+
+// d3 chart
+var chartGraph = function(element, data, opts) {
+  var width = opts.width || 200,
+      height = opts.height || 80,
+      padding = opts.padding || 30;
+
+  // chart
+  var svg     = d3.select(element[0])
+                  .append('svg:svg')
+                  .attr('width', width)
+                  .attr('height', height)
+                  .attr('class', 'sparkline')
+                  .append('g')
+                    .attr('transform', 'translate('+padding+', '+padding+')');
+
+  svg.selectAll('*').remove();
+
+  var maxY    = d3.max(data),
+      x       = d3.scale.linear()
+                  .domain([0, data.length])
+                  .range([0, width]),
+      y       = d3.scale.linear()
+                  .domain([0, maxY])
+                  .range([height, 0]),
+      yAxis = d3.svg.axis().scale(y)
+                    .orient('left')
+                    .ticks(5);
+
+  svg.append('g')
+      .attr('class', 'axis')
+      .call(yAxis);
+
+  var line    = d3.svg.line()
+                  .interpolate('linear')
+                  .x(function(d,i){return x(i);})
+                  .y(function(d,i){return y(d);}),
+      path    = svg.append('svg:path')
+                    .data([data])
+                    .attr('d', line)
+                    .attr('fill', 'none')
+                    .attr('stroke', 'black')
+                    .attr('stroke-width', '1');
+}
